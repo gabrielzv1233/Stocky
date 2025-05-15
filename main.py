62f755058851b8b7d56c7f7b3de2da1de2036dd1
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2025-05-14 19:49:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -504,4 +504,4 @@ $(document).on("click", ".uploaded-img", function(){
 """
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=443,debug=False)
+    app.run(host='0.0.0.0',port=80,debug=False)
